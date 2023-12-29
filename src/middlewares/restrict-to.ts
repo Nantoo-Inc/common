263b60c16c7744f619cda0eb01defa47874cac57
David Z Quartz <42542676+DavidQuartz@users.noreply.github.com>
2023-12-29 23:05:40 +0000

    fixed class instantiation

--- src/middlewares/restrict-to.ts
+++ src/middlewares/restrict-to.ts
@@ -5,7 +5,7 @@ export const restrictTo = (...roles: string[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
         // roles '4286' (admin), 'restaurant-manager', 'user'
         if (!roles.includes(req.currentUser!.role)) {
-            return next(new NotAuthorizedError);
+            return next(new NotAuthorizedError());
         }
 
         next();
